7e801c43c245494812d6cfc76b267c79fcf933f4
Chris Ko <ko_ka_chun@hotmail.com>
2022-08-18 12:44:57 +0100

    added suits to Card.

--- src/main/java/org/example/card/Card.java
+++ src/main/java/org/example/card/Card.java
@@ -5,16 +5,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Card {
+    List<String> suits;
     List<String> symbol;
     List<Integer> value;
 
     public Card() {
+        this.suits = new ArrayList<>(Arrays.asList("\u2660", "\u2665", "\u2666", "\u2663"));
         this.symbol = new ArrayList<>(Arrays.asList("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"));
         this.value = new ArrayList<>(Arrays.asList(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14));
     }
 
     public static void main(String[] args) {
         Card card = new Card();
+        System.out.println(card.suits);
         System.out.println(card.symbol);
         System.out.println(card.value);
     }
