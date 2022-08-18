a648ac9e55960900592c95b17951a32db7024b9b
Chris Ko <ko_ka_chun@hotmail.com>
2022-08-18 17:09:51 +0100

    created CardGame with getDeckOfCards method and modified Card.

--- src/main/java/org/example/card/Card.java
+++ src/main/java/org/example/card/Card.java
@@ -5,22 +5,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Card {
-    List<String> suits;
-    List<String> symbol;
-    List<Integer> value;
 
-    public Card() {
-        this.suits = new ArrayList<>(Arrays.asList("\u2660", "\u2665", "\u2666", "\u2663"));
-        this.symbol = new ArrayList<>(Arrays.asList("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"));
-        this.value = new ArrayList<>(Arrays.asList(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14));
+    private String suit;
+    private String symbol;
+    private int value;
+
+    public Card(String suit, String symbol, int value) {
+        this.suit = suit;
+        this.symbol = symbol;
+        this.value = value;
     }
 
-    public static void main(String[] args) {
-        Card card = new Card();
-        System.out.println(card.suits);
-        System.out.println(card.symbol);
-        System.out.println(card.value);
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + "(suit: " + this.suit + ", symbol: " + this.symbol + ", value: " + this.value + ")";
     }
-
-
 }
--- src/main/java/org/example/card/CardGame.java
+++ src/main/java/org/example/card/CardGame.java
@@ -1,6 +1,49 @@
 package org.example.card;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CardGame {
+    private final List<String> SUITS = new ArrayList<>(Arrays.asList("\u2660", "\u2665", "\u2666", "\u2663"));
+    private final List<String> SYMBOLS = new ArrayList<>(Arrays.asList("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"));
+    private final List<Integer> VALUE = new ArrayList<>(Arrays.asList(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14));
+    private List<Card> deckOfCards;
+
+    public CardGame() {
+        this.deckOfCards = this.createDeckOfCards();
+    }
+
+    public List<String> getSUITS() {
+        return SUITS;
+    }
+
+    public List<String> getSYMBOLS() {
+        return SYMBOLS;
+    }
+
+    public List<Integer> getVALUE() {
+        return VALUE;
+    }
+
+    public List<Card> getDeckOfCards() {
+        return deckOfCards;
+    }
+
+    public List<Card> createDeckOfCards() {
+        List<Card> deckOfCards = new ArrayList<>();
+        for (String suit : this.getSUITS()) {
+            for (int i=0; i < this.getSYMBOLS().size(); i++) {
+                deckOfCards.add(new Card(suit, this.getSYMBOLS().get(i), this.getVALUE().get(i)));
+            }
+        }
+        return deckOfCards;
+    }
+
+    public static void main(String[] args) {
+        CardGame cardGame = new CardGame();
+        System.out.println(cardGame.getDeckOfCards());
+    }
 
 
 }
