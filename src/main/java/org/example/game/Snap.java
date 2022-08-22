c7954e230b82415d0217d7b955189f470660ef72
Chris Ko <ko_ka_chun@hotmail.com>
2022-08-22 18:39:40 +0100

    added timer.

--- src/main/java/org/example/game/Snap.java
+++ src/main/java/org/example/game/Snap.java
@@ -6,6 +6,10 @@ import org.example.player.HumanPlayer;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
 public class Snap extends CardGame{
 
     private List<Card> cardsDrawn = new ArrayList<>();
@@ -30,6 +34,7 @@ public class Snap extends CardGame{
         System.out.println("Do you want to play Snap? Please type Y for YES and N for NO.");
         boolean isGameOn = false;
 
+
         if (user.getResponse().toLowerCase().equals("y")) {
             System.out.println("Let's play Snap!");
             isGameOn = true;
@@ -44,7 +49,27 @@ public class Snap extends CardGame{
             System.out.println(user.getName() + " draws: " + this.cardsDrawn.get(this.cardsDrawn.size() - 1));
 
             if (areTwoCardsHavingTheSameSymbol()) {
-                System.out.println("Congratulations! " + user.getName() + ", you win!");
+
+                System.out.println("Here is the snap opportunity");
+
+                // set up a task, i.e wait for user's response
+                TimerTask task = new TimerTask() {
+                    @Override
+                    public void run() {
+                        if (user.getResponse().toLowerCase().equals("snap") ) {
+                            System.out.println("Congratulations! " + user.getName() + ", you win!");
+                        } else {
+                            System.out.println("You miss the Snap opportunity :(");
+                        }
+
+                    }
+                };
+                // set up time allowed, e.g 2s
+                long timeForResponse = TimeUnit.SECONDS.toMillis(2);
+                // schedule the timer to run the task
+                Timer timer = new Timer();
+                timer.schedule(task, timeForResponse);
+
                 isGameOn = false;
                 return;
             }
